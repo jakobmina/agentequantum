dad077e3aac48b96aeae594060a2654b18e6d600
jakocrazykings <jakocrazykings@gmail.com>
2025-09-06 22:34:29 +0000

    structure

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+from audio_receptor import AudioQuantumStatePreparation
+from typing import List, Tuple, Dict, Any
+from datetime import datetime
+import numpy as np
+import pyaudio
+import wave
+import threading
+import time
+from scipy import signal
+from scipy.fft import fft, fftfreq
+import matplotlib.pyplot as plt
+from collections import deque
+import json
+
 def main():
-    print("Hello from myapp!")
-
-
+    audio_state_preparation = AudioQuantumStatePreparation()
+    
+AudioQuantumStatePreparation()
 if __name__ == "__main__":
     main()
+    audio_state_preparation.start_audio_capture()
